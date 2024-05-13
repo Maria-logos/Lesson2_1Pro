08bb20d0724d968f6d35a1b861ce738380cc9f35
Maria <msinukova@gmail.com>
2024-05-13 19:11:56 +0300

    Added converting from km to miles

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,9 +3,16 @@ package app;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final double CONV_M = 0.62137;
     public static void main(String[] args) {
 
-        System.out.println(" This is project for Converting Miles into Kilometers");
-
+            System.out.println("App for distance converting. ");
+            System.out.println("Version 1.0.");
+            double miles = 5;
+            double kms = convMilesToKolometers(miles);
+            System.out.println("Result is " + kms + " km.");
+        }
+        private static double convMilesToKolometers(double miles) {
+            return miles / CONV_M;
         }
     }
